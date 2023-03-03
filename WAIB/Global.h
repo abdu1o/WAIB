cdd71d204574bcb9a74bc59e8aaa7965fd654f23
Oleksandr Kapara <a.kapara2006@gmail.com>
2023-03-03 22:18:47 +0200

    add global

---
+++ WAIB/Global.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <conio.h>
+#include <iomanip>
+#include <windows.h> 
+#include <cmath>
+#pragma comment(lib, "winmm.lib")
+using namespace std;
--- WAIB/WAIB.cpp
+++ WAIB/WAIB.cpp
@@ -1,7 +1,24 @@
-﻿#include <iostream>
-using namespace std;
+﻿#include "Global.h"
 
 int main()
 {
-    cout << "Hello World!\n";
+	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    system("title WAIB");
+    
+	MoveWindow(GetConsoleWindow(), 220, 80, 800, 550, true);
+
+	CONSOLE_FONT_INFOEX font;
+
+	font.cbSize = sizeof(font);
+	font.dwFontSize.Y = 30;
+	font.FontFamily = FF_DONTCARE;
+	font.FontWeight = FW_NORMAL;
+
+	wcscpy_s(font.FaceName, 9, L"Algerian");
+	SetCurrentConsoleFontEx(h, 0, &font);
+
+	CONSOLE_CURSOR_INFO cursor;
+	cursor.bVisible = false;
+	cursor.dwSize = 1;
+	SetConsoleCursorInfo(h, &cursor);
 }
