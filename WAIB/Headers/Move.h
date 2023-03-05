0caf2ae95a7f9a21818a246ec921921917b7ad18
Oleksandr Kapara <a.kapara2006@gmail.com>
2023-03-05 19:58:38 +0200

    menu + move(need to test)

---
+++ WAIB/Headers/Move.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include "Headers/Global.h"
+
+class Move
+{
+private:
+	char emoji;
+	COORD Cursor;
+
+public:
+	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	Move(char _emoji, COORD _Cursor, int x, int y); // êîíñòðóêòîð 
+
+
+	void SetPosition(); // óñòàíîâêà êóðñîðà
+
+	void SetColor(int attribute); // óñòàíîâêà öâåòà 
+
+	void PrintEmoji(); // âûâîä ýìîäæè â êîíñîëü 
+
+
+	void MoveUp();
+	void MoveDown();
+	void MoveRight();
+	void MoveLeft();
+};
