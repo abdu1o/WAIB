907a3794d4a7fbf92eb291b4ae488c6b87f10a57
Oleksandr Kapara <a.kapara2006@gmail.com>
2023-03-05 20:26:27 +0200

    -Cpp file

--- WAIB/Headers/Move.h
+++ WAIB/Headers/Move.h
@@ -1,5 +1,5 @@
 ﻿#pragma once
-#include "Headers/Global.h"
+#include "Global.h"
 
 class Move
 {
@@ -10,18 +10,43 @@ private:
 public:
 	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
 
-	Move(char _emoji, COORD _Cursor, int x, int y); // êîíñòðóêòîð 
+	Move(char _emoji, COORD _Cursor, int x, int y) // êîíñòðóêòîð 
+	{
+		this->emoji = _emoji;
+		this->Cursor = _Cursor;
+		Cursor.X = x;
+		Cursor.Y = y;
+	}
 
+	void SetPosition()
+	{
+		SetConsoleCursorPosition(h, Cursor);
+	}
 
-	void SetPosition(); // óñòàíîâêà êóðñîðà
+	void SetColor(int attribute)
+	{
+		SetConsoleTextAttribute(h, attribute);
+	}
 
-	void SetColor(int attribute); // óñòàíîâêà öâåòà 
+	void PrintEmoji()
+	{
+		cout << emoji;
+	}
 
-	void PrintEmoji(); // âûâîä ýìîäæè â êîíñîëü 
-
-
-	void MoveUp();
-	void MoveDown();
-	void MoveRight();
-	void MoveLeft();
+	void MoveUp()
+	{
+		Cursor.Y--;
+	}
+	void MoveDown()
+	{
+		Cursor.Y++;
+	}
+	void MoveRight()
+	{
+		Cursor.X++;
+	}
+	void MoveLeft()
+	{
+		Cursor.X++;
+	}
 };
