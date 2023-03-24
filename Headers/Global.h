2328fb8359e89141b0e26689a4450d52c8ebf044
Oleksandr Kapara <103050489+abdu1o@users.noreply.github.com>
2023-03-24 12:37:03 +0200

    Delete Headers directory

--- Headers/Global.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-#include <string>
-#include <conio.h>
-#include <iomanip>
-#include <windows.h> 
-#include <cmath>
-#pragma comment(lib, "winmm.lib")
-using namespace std;
