4e6ea5b3a59fb7bcb053c3b97b85524aa3ee5aaa
Oleksandr Kapara <a.kapara2006@gmail.com>
2023-03-28 17:05:20 +0300

    small changes

--- WAIB/Headers/Global.h
+++ WAIB/Headers/Global.h
@@ -5,7 +5,6 @@
 #include <iomanip>
 #include <windows.h> 
 #include <cmath>
-#include "Enums/Enums.h"
 #pragma comment(lib, "winmm.lib")
 using namespace std;
 HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
