71f609c6b3d6e91cd32d6757158f71f040c81301
MX322 <mlemix32@gmail.com>
2023-03-28 17:29:37 +0300

    level update

--- WAIB/Headers/Level.h
+++ WAIB/Headers/Level.h
@@ -1,13 +1,14 @@
 #include "Global.h"
 #include "Enums/Enums.h"
+#include "Headers/Move.h"
 
 
 class LevelMap
 {
 
 private:
-    static const int WIDTH = 70;
-    static const int HEIGHT = 20;
+    static const int WIDTH = 67;
+    static const int HEIGHT = 23;
 
     static const int StartWIDTH = 10;
     static const int StartHEIGHT = 2;
@@ -72,6 +73,64 @@ public:
                 {
                     menu_box[y][x] = Border::EMPTY; // nihuya
                 }
+
+                int box_Y = 0;
+                int box_X = -10;
+                int steps = 0;
+
+
+                for (int i = 0; i < 9; i++)
+                {
+                    box_X += 18;
+
+                    if (x == EndWIDTH - 10 && y == StartHEIGHT + 7)
+                    {
+                        menu_box[y][x] = LevelBox::LEVEL_BORDER_LEFT_RIGHT;
+                    }
+
+                    if (x == StartWIDTH + 10 && y == StartHEIGHT + 13)
+                    {
+                        menu_box[y][x] = LevelBox::LEVEL_BORDER_LEFT_RIGHT;
+                    }
+
+                    else if (y == StartHEIGHT + 3 + box_Y && x == StartWIDTH + box_X)//3 7
+                    {
+                        menu_box[y][x] = LevelBox::LEVEL_CORNER_01;
+                    }
+
+                    else if (y == StartHEIGHT + 3 + box_Y && x == StartWIDTH + 4 + box_X)//3 7
+                    {
+                        menu_box[y][x] = LevelBox::LEVEL_CORNER_02;
+                    }
+
+                    else if (y == StartHEIGHT + 5 + box_Y && x == StartWIDTH + 4 + box_X)//3 7
+                    {
+                        menu_box[y][x] = LevelBox::LEVEL_CORNER_03;
+                    }
+
+                    else if (y == StartHEIGHT + 5 + box_Y && x == StartWIDTH + box_X)//3 7
+                    {
+                        menu_box[y][x] = LevelBox::LEVEL_CORNER_04;
+                    }
+
+                    else if (y == StartHEIGHT + 4 + box_Y && x == StartWIDTH + 2 + box_X)
+                    {
+                        menu_box[y][x] = 100 + steps + 1;
+                    }
+
+                    steps++;   
+
+
+                    if (steps % 3 == 0)
+                    {
+                        box_Y += 6;
+                        box_X = -10;
+                    }
+
+                    
+                }
+                
+               
             }
         }
     }
@@ -126,10 +185,109 @@ public:
                     cout << (char)Border::_CORNER_04;
                     break;
 
+                // LEVEL =========================================
+
+                case LevelBox::LEVEL_BORDER_LEFT_RIGHT:
+                    SetConsoleTextAttribute(h, DARKGRAY);
+                    cout << (char)Border::_BORDER_LEFT_RIGHT;
+                    break;
+
+                case LevelBox::LEVEL_BORDER_UP_DOWN:
+                    SetConsoleTextAttribute(h, DARKGRAY);
+                    cout << (char)Border::_BORDER_UP_DOWN;
+                    break;
+
+                case LevelBox::LEVEL_CORNER_01:
+                    SetConsoleTextAttribute(h, DARKGRAY);
+                    cout << (char)Border::_CORNER_01;
+                    break;
+
+                case LevelBox::LEVEL_CORNER_02:
+                    SetConsoleTextAttribute(h, DARKGRAY);
+                    cout << (char)Border::_CORNER_02;
+                    break;
+
+                case LevelBox::LEVEL_CORNER_03:
+                    SetConsoleTextAttribute(h, DARKGRAY);
+                    cout << (char)Border::_CORNER_03;
+                    break;
+
+                case LevelBox::LEVEL_CORNER_04:
+                    SetConsoleTextAttribute(h, DARKGRAY);
+                    cout << (char)Border::_CORNER_04;
+                    break;
+
+                case 101: // Level 1
+                    SetConsoleTextAttribute(h, WHITE);
+                    cout << "1";
+                    break;
+
+                case 102: // Level 2
+                    SetConsoleTextAttribute(h, WHITE);
+                    cout << "2";
+                    break;
+
+                case 103: // Level 3
+                    SetConsoleTextAttribute(h, WHITE);
+                    cout << "3";
+                    break;
+
+                case 104: // Level 4
+                    SetConsoleTextAttribute(h, WHITE);
+                    cout << "4";
+                    break;
+
+                case 105: // Level 5
+                    SetConsoleTextAttribute(h, WHITE);
+                    cout << "5";
+                    break;
+
+                case 106: // Level 6 
+                    SetConsoleTextAttribute(h, WHITE);
+                    cout << "6";
+                    break;
+
+                case 107: // Level 7
+                    SetConsoleTextAttribute(h, WHITE);
+                    cout << "7";
+                    break;
+
+                case 108: // Level 8 
+                    SetConsoleTextAttribute(h, WHITE);
+                    cout << "8";
+                    break;
+
+                case 109: // Level 9
+                    SetConsoleTextAttribute(h, WHITE);
+                    cout << "9";
+                    break;
+
+
                 }
             }
             cout << "\n";
         } 
         cout << "\n";
+
+        // Movement ====================================================
+
+        Move Cursor = { NULL, StartWIDTH, StartHEIGHT };
+        Cursor.SetPosition();
+
+        while (true)
+        {
+            if (_kbhit())
+            {
+                int code = _getch();
+
+                if (code == 224)
+                {
+                    code = _getch();
+                }
+
+
+            }
+        }
+
     }
 };
