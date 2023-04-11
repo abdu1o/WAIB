77c23db43b2826b2dde5160e771cf12fc778fb69
MX322 <mlemix32@gmail.com>
2023-04-12 00:28:06 +0300

    update

---
+++ Headers/Move.h
@@ -0,0 +1,63 @@
+﻿#pragma once
+#include "Global.h"
+
+class Move
+{
+private:
+	char emoji;
+	COORD Cursor{};
+
+public:
+	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	Move(){}
+
+	Move(char _emoji, int x, int y)
+	{
+		this->emoji = _emoji;
+		Cursor.X = x;
+		Cursor.Y = y;
+	}
+
+	void SetPosition()
+	{
+		SetConsoleCursorPosition(h, Cursor);
+	}
+
+	void SetColor(int attribute)
+	{
+		SetConsoleTextAttribute(h, attribute);
+	}
+
+	void PrintEmoji()
+	{
+		cout << emoji;
+	}
+
+	void MoveUp()
+	{
+		Cursor.Y--;
+	}
+	void MoveDown()
+	{
+		Cursor.Y++;
+	}
+	void MoveRight()
+	{
+		Cursor.X++;
+	}
+	void MoveLeft()
+	{
+		Cursor.X--;
+	}
+
+	short GetY()
+	{
+		return Cursor.Y;
+	}
+	short GetX()
+	{
+		return Cursor.X;
+	}
+
+};
